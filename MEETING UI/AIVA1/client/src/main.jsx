81ed90c8b042ad34e3856b5937368cd176d7dc42
PritishKhokrale <12.pritish.khokrale@gmail.com>
2026-04-19 16:53:37 +0530

    Fix blank screen crash caused by missing GoogleOAuthProvider bounding box

--- MEETING UI/AIVA1/client/src/main.jsx
+++ MEETING UI/AIVA1/client/src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
@@ -8,8 +9,10 @@ import { AuthProvider } from './context/AuthContext.jsx'
 // causes duplicate WebRTC socket connections (triple-video bug).
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || "1064037786827-dummy.apps.googleusercontent.com"}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </GoogleOAuthProvider>
   </BrowserRouter>
 )
